aa85c47c997f85b04dfc8e48fe59c137dcfb1be2
UtkarshaGN <utkarsha.gajanan@gmail.com>
2024-01-17 13:43:27 +1000

    my first commit

---
+++ vite-project/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className='main lg:flex md:flex flex-wrap justify-between px-3 bg-[#40407a] py-4 items-center shadow-md sticky top-0'>
+            <div className='left flex items-center space-x-3 justify-center'>
+                <img className ="w-10"src="https://movie-app-88kamal.vercel.app/clipart3105859.png" alt="img" />
+                <h1 className='font-bold text-2xl text-white ' >Movieapp</h1>
+            </div>
+            <div className='right'>
+                <ul className='flex space-x-4 text-xl text-white justify-center'>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Services</li>
+                </ul>
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
